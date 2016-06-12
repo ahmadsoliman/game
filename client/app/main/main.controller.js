a1f9055ffa27e0cfe4f6e4c797b51998ab1747d5
Ahmad Soliman <ahmadadel92@gmail.com>
2016-06-12 15:35:26 +0200

    more work towards game structure and filling in some static data

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -5,34 +5,37 @@
   class MainController {
 
     constructor($http) {
+      this.vm = this;
       this.$http = $http;
-      this.awesomeThings = [];
+      // this.awesomeThings = [];
+      
     }
 
     $onInit() {
-      this.$http.get('/api/things')
-        .then(response => {
-          this.awesomeThings = response.data;
-        });
+      // this.$http.get('/api/things')
+      //   .then(response => {
+      //     this.awesomeThings = response.data;
+      //   });
     }
 
-    addThing() {
-      if (this.newThing) {
-        this.$http.post('/api/things', {
-          name: this.newThing
-        });
-        this.newThing = '';
-      }
-    }
-
-    deleteThing(thing) {
-      this.$http.delete('/api/things/' + thing._id);
-    }
+    // addThing() {
+    //   if (this.newThing) {
+    //     this.$http.post('/api/things', {
+    //       name: this.newThing
+    //     });
+    //     this.newThing = '';
+    //   }
+    // }
+    //
+    // deleteThing(thing) {
+    //   this.$http.delete('/api/things/' + thing._id);
+    // }
   }
 
   angular.module('gameApp')
     .component('main', {
       templateUrl: 'app/main/main.html',
-      controller: MainController
+      controller: MainController,
+      controllerAs: 'vm'
     });
 })();

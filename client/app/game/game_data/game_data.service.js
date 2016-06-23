ee6a9ada9ff733e0178d8fadd39d2b701c6e00c5
Ahmad Soliman <ahmadadel92@gmail.com>
2016-06-23 14:47:36 +0200

    added suspense logic nd ui

--- client/app/game/game_data/game_data.service.js
+++ client/app/game/game_data/game_data.service.js
@@ -12,14 +12,19 @@ angular.module('gameApp')
       { code: 2, name: 'wedding', time: 3*52, budget: 40000}
     ];
 
+    var suspenseRanges = [
+      {name: 'rent', text: 'Rent or Mortgage Repayments', code: 'S1',  min: 50, max: 2000, value: 500, freq: 1},
+      {name: 'mobile', text: 'Mobile and Internet', code: 'S2', min: 10, max: 200, value: 50, freq: 1},
+      {name: 'utilities', text: 'Utilities', code: 'S3', min: 10, max: 200, value: 50, freq: 1},
+      {name: 'health', text: 'Health and Other Insurance', code: 'S4', min: 10, max: 200, value: 50, freq: 1},
+      {name: 'commitments', text: 'Other Regular Commitments', code: 'S5', min: 10, max: 200, value: 50, freq: 1}
+    ];
 
     return {
       user: user,
       targets: targets,
-      graph: graph
+      suspenseRanges: suspenseRanges
     };
 
-    function graph() {
-      console.log('GRAPH!');
-    }
+
   });
--- client/app/game/suspense/suspense.controller.js
+++ client/app/game/suspense/suspense.controller.js
@@ -1,8 +1,31 @@
 'use strict';
 
 class SuspenseController {
-  constructor() {
+  constructor($timeout, gameData) {
+    var vm = this;
+    vm.gameData = gameData;
 
+    vm.ranges = vm.gameData.suspenseRanges;
+
+    angular.forEach(vm.ranges, (r, i) => {
+      $timeout(() => {
+        $('#' + r.name + '-range').range({
+            min: r.min,
+            max: r.max,
+            start: r.value,
+            step: 10,
+            onChange: (val) => { $timeout(() => vm.ranges[i].value = val); }
+        });
+
+        $('#' + r.name + '-freq').dropdown({
+          action: 'activate',
+          onChange: function(value, text, $selectedItem) {
+            $timeout(() => vm.ranges[i].freq = value);
+          }
+        });
+      })
+
+    });
   }
 }
 

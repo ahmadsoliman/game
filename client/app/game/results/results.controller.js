45c458d605ee1090b754a8020aa18b76f5e0f186
Ahmad Soliman <ahmadadel92@gmail.com>
2016-07-03 21:10:49 +0200

    added results ui

--- client/app/game/results/results.controller.js
+++ client/app/game/results/results.controller.js
@@ -5,7 +5,11 @@ class ResultsController {
     var vm = this;
     vm.gameData = gameData;
 
-
+    vm.results = [
+      { name: 'discretion', text: 'Discretion', value: vm.gameData.getDSum() },
+      { name: 'suspense', text: 'Suspense', value: vm.gameData.getSSum() },
+      { name: 'reserve', text: 'Reserve', value: vm.gameData.getR() }
+    ];
   }
 }
 
